04d533be4c8dca855d6146306e80c193fb017a7f
Edwards Kenway <edwardsrian8@gmail.com>
2025-09-18 12:04:08 +1000

    Finished Lab 7

---
+++ src/Person.java
@@ -0,0 +1,22 @@
+public abstract class Person {
+    
+    protected String name;
+    protected Hand hand;
+
+    public Person(String name) {
+        this.name = name;
+        this.hand = new Hand();
+    }
+
+    public void drawCard(Deck deck) {
+        hand.add(deck.removeCard());
+        if (hand.busted())
+            System.out.println(name + " busts with " + hand + "!");
+    }
+
+    public String toString() {
+        return name + " has " + hand;
+    }
+
+    public abstract void haveTurn(Deck deck);
+}
